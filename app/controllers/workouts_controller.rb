08f88171d0de0756bc4c8b8ce49c84f1cde3eec0
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-04-27 15:09:20 -0700

    add full CRUD

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,12 +1,17 @@
 class WorkoutsController < ApplicationController
-  before_action :admin_check, except: [:index, :show]
+  before_action :admin_check, except: [:index, :home]
 
   def index
     @workouts = Workout.all.where("DATE(time - INTERVAL '7 hour') >= ?", Date.today).order("time ASC")
   end
 
-  def show
+  def home
     @workouts = Workout.where("DATE(time - INTERVAL '7 hour') = ?", Date.today)
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+    @clients = @workout.clients
   end
 
   def new
@@ -21,6 +26,25 @@ class WorkoutsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+    if @workout.update_attributes(workout_params)
+        redirect_to classes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @workout = Workout.find(params[:id])
+    @workout.destroy
+    redirect_to classes_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'workouts#show'
+  root 'workouts#home'
 
   resources :clients
 
   resources :instructors, except: [:edit, :update]
 
-  resources :workouts, except: [:show, :edit, :update]
+  resources :workouts
 
   get '/login', to: 'sessions#new'
 
